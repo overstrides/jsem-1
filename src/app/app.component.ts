bb7f3d7ed871b2e18a5d3b0aedc7a39d5f93fecf
Александр Шут <overstrides@gmail.com>
2021-11-12 14:56:16 +0200

    1st lessong

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'users-list';
+  typesOfShoes: string[] = ['Test', 'Boots', 'Clogs', 'Loafers', 'Moccasins', 'Sneakers'];
 }
